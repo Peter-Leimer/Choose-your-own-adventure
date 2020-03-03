6fde2223a268a80b61141f99475626bec9cdb52b
Peter-Leimer <peterleimer6@gmail.com>
2020-03-03 08:47:39 -0600

    first commit

---
+++ Adventure-Main.py
@@ -0,0 +1,7 @@
+# Main
+
+#Intro
+Health=100
+name = str(input("Enter your character name: "))
+print(f"The Mighty {name} has entered the Labyrinth ....")
+print(f"The Labyrinth is filled with danger, prepare your self OR DIE !!!!!")
