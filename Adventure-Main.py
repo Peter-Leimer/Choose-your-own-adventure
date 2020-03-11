27727b5ddc81b9e6249d68f91f648525c40261a7
Peter-Leimer <peterleimer6@gmail.com>
2020-03-11 08:48:41 -0500

    New Room started

--- Adventure-Main.py
+++ Adventure-Main.py
@@ -1,9 +1,14 @@
+import random
+
 # Main
 #Intro
 global Health
 global name
 global Monsters
 global traps
+global treasureNum
+global treasures
+treasureNum=0
 
 def intro():
     Health=100
@@ -20,6 +25,7 @@ def monsters():
     Monsters.append("Gremlin")
     Monsters.append("Goblin ")
     Monsters.append("Dragon Hatchling")
+    Monsters.append("Skeleton")
     print(Monsters[0])
     print(Monsters[1])
     print(Monsters[2])
@@ -27,7 +33,24 @@ def monsters():
 
 def Traps():
     traps=[]
-    traps.append("bottomless pit")
+    traps.append("Bottomless pit")
+    traps.append("Poison dart trap")
+    traps.append("Spike trap")
+    traps.append("A fuking Rue Goldberg Machine, with FIRE!")
+    
+def treasures():
+    treasures=[]
+    treasures.append("Holy Grail")
+    treasures.append("Sword of Damocles")
+    treasures.append("Ark of the Covenant")
+
+def NewRoom():
+    choose = int(input("You must choose a Path at a fork in the labrinyth, at your own risk (hit 1 or 2)"))
+    if (choose==1):
+        print("path1")
+    elif (choose==2):
+        print("path2")
 
 intro()
-monsters()
+NewRoom()
+#monsters()
